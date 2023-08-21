a05c39b615db3ec18ca809035f20679285f6e6ad
Ribhav Ojha <ribhav.ojha@student.manchester.ac.uk>
2023-08-21 17:54:46 +0100

    Add a DQN agent

--- Reinforcement Learning/cartpole.py
+++ Reinforcement Learning/cartpole.py
@@ -10,18 +10,29 @@ from rl.memory import SequentialMemory
 
 
 env = gym.make("CartPole-v1")
-# Random moves
-episodes = 10
-for i in range(1, episodes + 1):
-    state = env.reset()
-    done = False
-    score = 0
-    while not done:
-        #action
-        action = random.choice([0,1])
-        n_state ,reward, done, info = env.step(action)
-        env.render()
-        score += reward
-        
-    print('Episode:{} Score:{}'.format(i, score))
+states = env.observation_space.shape[0]
+actions = env.action_space.n    
+# del model
+model = Sequential()
+model.add(Flatten(input_shape=(1,states)))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(1024, activation='relu'))
+model.add(Dense(actions, activation='linear'))
+# model.add(Flatten())
+model.summary()
+agent = DQNAgent(model=model, 
+                memory=SequentialMemory(limit = 500000, window_length = 1), 
+                policy=BoltzmannQPolicy(),
+                nb_actions=actions,
+                nb_steps_warmup=10, 
+                target_model_update=1e-2)   
+
+agent.compile(Adam(lr=1e-3), metrics=['mae'])
+agent.fit(env, nb_steps=10000, visualize=False, verbose=1)
+
+results = agent.test(env, nb_episodes=10, visualize=True)
+print(np.mean(results.history['episode_reward']))
 env.close()
+
+
+
